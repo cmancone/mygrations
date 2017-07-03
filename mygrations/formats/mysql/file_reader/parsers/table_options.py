4dff94d90c75963e7f4409e1a002a95602e537c4
Conor Mancone <cmancone@gmail.com>
2017-07-03 18:03:38 -0400

    Continuing to test mysql parsers

--- mygrations/formats/mysql/file_reader/parsers/table_options.py
+++ mygrations/formats/mysql/file_reader/parsers/table_options.py
@@ -7,12 +7,12 @@ class table_options( parser ):
 
     # UNIQUE account_id (account_id)
     rules = [
-        { 'type': 'regexp', 'value': '[^=]{1,25}', 'name': 'option' },
+        { 'type': 'regexp', 'value': '[^=]{1,30}', 'name': 'name' },
         { 'type': 'literal', 'value': '=' },
         { 'type': 'regexp', 'value': '[^\s;]+', 'name': 'value' }
     ]
 
     def process( self ):
 
-        self.name = self._values['name']
-        self.value = self._values['value']
+        self.name = self._values['name'].strip()
+        self.value = self._values['value'].strip()
