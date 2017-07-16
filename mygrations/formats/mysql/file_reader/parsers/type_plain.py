5e7826063a3d64bcb24096ed4b7eab2a3dfd08d7
Conor Mancone <cmancone@gmail.com>
2017-07-16 09:25:21 -0400

    Tests for type plain

--- mygrations/formats/mysql/file_reader/parsers/type_plain.py
+++ mygrations/formats/mysql/file_reader/parsers/type_plain.py
@@ -8,12 +8,14 @@ class type_plain( parser ):
     column_type = ''
     default = ''
     has_comma = False
+    null = False
 
     # created date
     rules = [
         { 'type': 'regexp', 'value': '[^\(\s\)]+', 'name': 'name' },
         { 'type': 'regexp', 'value': '\w+', 'name': 'type' },
-        { 'type': 'regexp', 'value': 'DEFAULT [^\(\s\)]+', 'optional': True, 'name': 'default' },
+        { 'type': 'literal', 'value': 'NOT NULL', 'optional': True },
+        { 'type': 'regexp', 'value': 'DEFAULT ([^\(\s\),]+)', 'optional': True, 'name': 'default' },
         { 'type': 'literal', 'value': ',', 'optional': True, 'name': 'ending_comma' }
     ]
 
@@ -23,4 +25,12 @@ class type_plain( parser ):
 
         self.name = self._values['name']
         self.column_type = self._values['type']
-        self.default = self._values['default'] if 'default' in self._values else ''
+        self.default = self._values['default'].strip( "'" ) if 'default' in self._values else None
+        self.null = False if 'NOT NULL' in self._values else True
+
+        # make sense of the default
+        if self.default and self.default.lower() == 'null':
+            self.default = None
+
+        if self.default is None and not self.null:
+            self.warnings.append( 'Column %s is not null and has no default: you should set a default to avoid MySQL warnings' % ( self.name ) )
