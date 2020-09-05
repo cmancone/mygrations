bb49b244088fcbae2b30a56d6f80d73cfbbbbb28
Conor Mancone <cmancone@gmail.com>
2020-09-05 19:29:51 -0400

    Options

---
+++ mygrations/core/definitions/option_test.py
@@ -0,0 +1,10 @@
+import unittest
+from .option import Option
+
+
+class TestOption(unittest.TestCase):
+    def test_can_create(self):
+        option = Option('my_option', 'my_value')
+
+        self.assertEquals('my_option', option.name)
+        self.assertEquals('my_value', option.value)
